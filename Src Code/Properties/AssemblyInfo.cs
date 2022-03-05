6879779025485519cd86a237c1e2c689002ea47a
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-03-05 13:54:39 +0300

    Add files via upload

---
+++ Src Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: Guid("e784b66b-066f-488d-9f3e-4cce16106b40")]
+[assembly: AssemblyTitle("Chaos Ransomware v4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Chaos Ransomware Builder v4")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
